570cbcbfc517e63183d01796f6d725837c23e75c
Ajay Kumar <ajay.kumar@softobiz.com>
2022-05-19 12:30:58 +0530

    data validators done

--- models/toursModel.js
+++ models/toursModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const slugify = require("slugify");
+const validator = require("validator");
 
 const tourSchema = new mongoose.Schema(
     {
@@ -8,6 +9,15 @@ const tourSchema = new mongoose.Schema(
             required: [true, "A tour must have a name"],
             unique: true,
             trim: true,
+            maxlength: [
+                40,
+                "A tour name must have less or equal 40 characters",
+            ],
+            minlength: [
+                10,
+                "A tour name must have more or equal 10 characters",
+            ],
+            // validate: [validator.isAlpha,"Tour name must only conatains characters"],
         },
         slug: String,
         duration: {
@@ -21,10 +31,16 @@ const tourSchema = new mongoose.Schema(
         difficulty: {
             type: String,
             required: [true, "A tour must have a difficulty"],
+            enum: {
+                values: ["easy", "medium", "difficult"],
+                message: "Difficulty should be easy, medium or difficult",
+            },
         },
         ratingsAverage: {
             type: Number,
             default: 4.5,
+            min: [1, "Rating must be above 1.0"],
+            max: [5, "Rating must be below 5.0"],
         },
         ratingsQuantity: {
             type: Number,
@@ -34,7 +50,17 @@ const tourSchema = new mongoose.Schema(
             type: Number,
             required: [true, "A tour must have a price"],
         },
-        priceDiscount: Number,
+        priceDiscount: {
+            type: Number,
+            validate: {
+                validator: function (val) {
+                    // this only work with create not for update
+                    return val < this.price;
+                },
+                message:
+                    "Discount price (${VALUE}) should be less than regular price",
+            },
+        },
         summary: {
             type: String,
             trim: true,
