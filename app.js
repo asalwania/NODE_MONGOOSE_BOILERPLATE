200a4369f8df011c3480abf3715b2642b7ec6053
Ajay Kumar <ajay.kumar@softobiz.com>
2022-05-19 13:37:02 +0530

    global error handler

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const morgan = require("morgan");
 
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
 
 const app = express();
 if (process.env.NODE_ENV === "development") {
@@ -23,11 +25,21 @@ app.use("/api/v1/users", userRouter);
 
 // handle unknown routes
 app.all("*", (req, res, next) => {
-    res.status(404).json({
-        status: "fail",
-        message: `Can't find ${req.originalUrl}`,
-    });
+    // res.status(404).json({
+    //     status: "fail",
+    //     message: `Can't find ${req.originalUrl}`,
+    // });
+
+    // const err = new Error(`Can't find ${req.originalUrl}`);
+    // err.status = "fail";
+    // err.statusCode = 404;
+    // next(err);
+
+    next(new AppError(`Can't find ${req.originalUrl} on this server )-:`, 404));
 });
+
+// globle error handling
+app.use(globalErrorHandler);
 
 module.exports = app;
 
