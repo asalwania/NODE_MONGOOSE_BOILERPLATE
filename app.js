83707484475fe2499ce6afc5981677579573a417
Ajay Kumar <ajay.kumar@softobiz.com>
2022-05-19 12:51:31 +0530

    unhandled routs

--- app.js
+++ app.js
@@ -13,13 +13,21 @@ if (process.env.NODE_ENV === "development") {
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
-app.use((req, res, next) => {
-    console.log("hello from middleware");
-    next();
-});
+// app.use((req, res, next) => {
+//     console.log("hello from middleware");
+//     next();
+// });
 
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
+
+// handle unknown routes
+app.all("*", (req, res, next) => {
+    res.status(404).json({
+        status: "fail",
+        message: `Can't find ${req.originalUrl}`,
+    });
+});
 
 module.exports = app;
 
