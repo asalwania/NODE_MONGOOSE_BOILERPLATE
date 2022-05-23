59af97bc124cfcba4c0f7881ec831d9f54a5afe7
Ajay Kumar <ajay.kumar@softobiz.com>
2022-05-23 13:31:58 +0530

    error handling

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,15 @@
+const AppError = require("../utils/appError");
+
+const handleCastErrorDB = (err) => {
+    const message = `Invalid ${err.path} : ${err.value}`;
+    return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+    const message = `Duplicate field value: (${err.keyValue.name}). Please use another value`
+    return new AppError(message,400)
+}
+
 const sendErrorDev = (err, res) => {
     res.status(err.statusCode).json({
         status: err.status,
@@ -8,10 +20,22 @@ const sendErrorDev = (err, res) => {
 };
 
 const sendErrorProd = (err, res) => {
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message,
-    });
+    // Operatoinal, trusted error: send message to client
+    if (err.isOperational) {
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message,
+        });
+    } else {
+        // Programming or other unknown error: don't leak error details
+        // 1) log the error
+        console.error("Error ", err);
+        // 2) Send generic message
+        res.status(500).json({
+            status: "error",
+            message: "somthing went very wrong!",
+        });
+    }
 };
 
 module.exports = (err, req, res, next) => {
@@ -20,7 +44,10 @@ module.exports = (err, req, res, next) => {
 
     if (process.env.NODE_ENV === "development") {
         sendErrorDev(err, res);
-    } else if (process.env.NODE_ENV === "production") {
-        sendErrorProd(err, res);
+    } else if (process.env.NODE_ENV.trim() === "production") {
+        let error = { ...err };
+        if (error.kind ==="ObjectId" ) error = handleCastErrorDB(error);
+        if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+        sendErrorProd(error, res);
     }
 };
