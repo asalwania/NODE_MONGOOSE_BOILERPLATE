eedaf8961fd2124acbdb7ebe6b317471772be1d9
Ajay Kumar <ajay.kumar@softobiz.com>
2022-05-27 16:34:26 +0530

    1. Role permissions

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,12 +11,13 @@ const signToken = (id) => {
 };
 
 exports.signup = catchAsync(async (req, res, next) => {
-    const { name, email, password, passwordConfirm } = req.body;
+    const { name, email, password, passwordConfirm, role } = req.body;
     const newUser = await User.create({
         name,
         email,
         password,
         passwordConfirm,
+        role,
     });
 
     const token = signToken(newUser._id);
@@ -53,6 +54,7 @@ exports.login = catchAsync(async (req, res, next) => {
     });
 });
 
+// middleWares
 exports.protect = catchAsync(async (req, res, next) => {
     // 1) Get and check the token
     let token;
@@ -99,3 +101,17 @@ exports.protect = catchAsync(async (req, res, next) => {
     req.user = currentUser;
     next();
 });
+
+exports.restrictTo = (...roles) => {
+    return (req, res, next) => {
+        if (!roles.includes(req.user.role)) {
+            return next(
+                new AppError(
+                    "You do not have permission to perform this action",
+                    403
+                )
+            );
+        }
+        next();
+    };
+};
